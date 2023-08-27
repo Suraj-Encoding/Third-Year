d0dbcc3afd67aa7f1be4a11fc38f0d0e6116a295
Suraj-Encoding <dalvisuraj256@gmail.com>
2023-08-27 08:51:33 +0530

    DAA added

--- DAA/Assignments/ass1.java
+++ DAA/Assignments/ass1.java
@@ -1,19 +1,19 @@
 // # Assignment-1 : Karatsuba Algorithm
 // # Square of 20 digit number using divide and conquer method
-// # Time : O(n^1.58)
+// # Time : O(n^1.59)
 import java.util.Scanner;
 
 public class ass1 {
    public static int karatsuba(int x, int y) {
-      if (x < 10 || y < 10) {
+      if (x < 10) {
          return x * y;
       } else {
          int n = Integer.toString(x).length();
          int h = n / 2;
          int a, b, c, d, ac, bd, plus, p, k;
          p = (int) (Math.pow(10, h));
-         k = (int) (Math.pow(10, n));
-         // Formula : ac*10^n + (ad+bc)*10^(n/2) + bd
+         k = (int) (Math.pow(10, (h * 2)));
+         // Formula : ac*10^(2*n/2) + (ad+bc)*10^(n/2) + bd
          a = x / p;
          b = x % p;
          c = y / p;
--- DAA/Assignments/ass1.py
+++ DAA/Assignments/ass1.py
@@ -1,25 +1,28 @@
 # Assignment-1 : Karatsuba Algorithm
 # Square of 20 digit number using divide and conquer method
-# Time : O(n^1.58)
+# Time : O(n^1.59)
 
+# Function
 def karatsuba(x,y):
     if x < 10:
-        return x*y
+        return x * y
     else:
         n = len(str(x))
         h = n // 2
         p = 10 ** h
-        k = 10 ** n
+        k = 10 ** (h * 2)
         a = x // p
         b = x % p
         c = y // p
         d = y % p
         ac = karatsuba(a, c)
         bd = karatsuba(b, d)
-        plus = karatsuba(a+b, c+d) - ac - bd
+        plus = karatsuba(a+b, c+d) - (ac + bd)
         return ac * k + plus * p + bd
+        # Formula : ac*10^(2*n/2) + (ad+bc)*10^(n/2) + bd 
 
-
+# Main
 x = input("Enter number: ")
 res = karatsuba(int(x), int(x))
 print("Square of " + str(x) + ": " + str(res))
+
